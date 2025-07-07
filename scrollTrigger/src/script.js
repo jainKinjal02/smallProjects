2342fb1300566d02ec0fa2ff83a46e40fd6a21ab
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-07 20:53:47 +0530

    DOMContentLoaded event listener setup

---
+++ scrollTrigger/src/script.js
@@ -0,0 +1,15 @@
+import { setupMarqueeAnimation } from "./marquee";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import Lenis from "lenis";
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger, SplitText);
+
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => lenis.raf(time * 1000));
+  gsap.ticker.lagSmoothing(0);
+});
