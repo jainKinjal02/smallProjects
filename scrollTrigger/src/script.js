3b94076369e65a4f15b1830e6c62a09d150b9e26
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-07 21:01:04 +0530

    created two utility function

--- scrollTrigger/src/script.js
+++ scrollTrigger/src/script.js
@@ -12,4 +12,46 @@ document.addEventListener("DOMContentLoaded", () => {
   lenis.on("scroll", ScrollTrigger.update);
   gsap.ticker.add((time) => lenis.raf(time * 1000));
   gsap.ticker.lagSmoothing(0);
+
+  const cards = gsap.utils.toArray(".card");
+  const introCard = cards[0];
+
+  const titles = gsap.utils.toArray(".card-title h1");
+  titles.forEach((title) => {
+    const split = new SplitText(title, {
+      type: "char",
+      charsClass: "char",
+      tag: "div",
+    });
+    split.chars.forEach((char) => {
+      char.innerHTML = `<span>${char.textContent}</span>`;
+    });
+  });
+
+  const cardImgWrapper = introCard.querySelector(".card-img");
+  const cardImg = introCard.querySelector(".card-img img");
+  gsap.set(cardImgWrapper, { scale: 0.5, borderRadius: "400px" });
+  gsap.set(cardImg, { scale: 1.5 });
+
+  function animateContentIn(titleChars, description) {
+    gsap.to(titleChars, { x: "0%", duration: 0.75, ease: "power4.out" });
+    gsap.to(description, {
+      x: 0,
+      opacity: 1,
+      duration: 0.75,
+      delay: 0.1,
+      ease: "power4.out",
+    });
+  }
+
+  function animateContentOut(titleChars, description) {
+    gsap.to(titleChars, { x: "0%", duration: 0.5, ease: "power4.out" });
+    gsap.to(description, {
+      x: "40px",
+      opacity: 0,
+      duration: 0.5,
+      ease: "power4.out",
+    });
+  }
+  
 });
